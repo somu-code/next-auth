28e1962bc95e5b4be98e5639e36a91b98d72532e
Somnath Golui <code.somu@gmail.com>
2023-09-03 10:14:34 +0530

    Now staticly generating users page

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -1,5 +1,13 @@
-import { getUserById } from "@/lib/prisma/users";
+import { getUserById, getUsers } from "@/lib/prisma/users";
 import User from "./user";
+
+export async function generateStaticParams() {
+  const { users } = await getUsers();
+
+  return users?.map((user) => {
+    userId: user.id;
+  });
+}
 
 export default async function Page({ params }: { params: { userId: string } }) {
   const { user } = await getUserById(params.userId);
