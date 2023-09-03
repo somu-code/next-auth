be68f719e395037ff0810071ddafe1127f859f03
Somnath Golui <code.somu@gmail.com>
2023-09-03 09:13:03 +0530

    added error.tsx

---
+++ src/app/users/error.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+  return (
+    <div>
+      <button onClick={() => reset()}>Reset error boundary</button>
+    </div>
+  );
+}
